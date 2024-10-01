e5d8f2f0163d339f3493f18a75abbd475881f925
Sweets Sweetman <sweetmantech@gmail.com>
2024-10-01 20:35:27 -0300

    I add working Suggestions.

--- components/LandingPage/Suggestions.tsx
+++ components/LandingPage/Suggestions.tsx
@@ -1,9 +1,32 @@
 import Image from "next/image";
+import { useChatProvider } from "@/providers/ChatProvider";
+import { Button } from "@/components/ui/Button";
+
+const promptOne = "How many tokens have I created this week?";
 
 const Suggestions = () => {
+  const { append } = useChatProvider();
+
+  const onSubmit = async (message: string) => {
+    append({ role: "user", content: message });
+  };
+
   return (
-    <div className="rounded-full overflow-hidden">
-      <Image src="/myco-logo.png" alt="Mushroom logo" width={80} height={80} />
+    <div className="flex flex-col items-center space-y-4">
+      <div className="rounded-full overflow-hidden">
+        <Image
+          src="/myco-logo.png"
+          alt="Mushroom logo"
+          width={80}
+          height={80}
+        />
+      </div>
+      <Button
+        onClick={() => onSubmit(promptOne)}
+        className="text-black border border-black"
+      >
+        {promptOne}
+      </Button>
     </div>
   );
 };
--- hooks/useChat.tsx
+++ hooks/useChat.tsx
@@ -2,13 +2,21 @@ import { useCsrfToken } from "@/packages/shared/src/hooks";
 import { useChat as useAiChat } from "ai/react";
 import { useQueryClient } from "@tanstack/react-query";
 import { useAccount } from "wagmi";
+import useConnectWallet from "./useConnectWallet";
 
 const useChat = () => {
+  const { connectWallet } = useConnectWallet();
   const csrfToken = useCsrfToken();
   const accountId = "3664dcb4-164f-4566-8e7c-20b2c93f9951";
   const queryClient = useQueryClient();
   const { address } = useAccount();
-  const { messages, input, handleInputChange, handleSubmit } = useAiChat({
+  const {
+    messages,
+    input,
+    handleInputChange,
+    handleSubmit: handleAiChatSubmit,
+    append,
+  } = useAiChat({
     api: `/api/chat`,
     headers: {
       "X-CSRF-Token": csrfToken,
@@ -25,7 +33,16 @@ const useChat = () => {
     },
   });
 
-  return { messages, input, handleInputChange, handleSubmit };
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    if (!address) {
+      e.preventDefault();
+      await connectWallet();
+      return;
+    }
+    handleAiChatSubmit(e);
+  };
+
+  return { messages, input, handleInputChange, handleSubmit, append };
 };
 
 export default useChat;
