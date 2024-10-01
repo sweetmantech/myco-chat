e1fe855312ba25ce64d94d2cb1e2dec9836c2745
Sweets Sweetman <sweetmantech@gmail.com>
2024-09-30 22:27:16 -0300

    I init <ChatProvider />

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -1,40 +1,20 @@
 import ChatInput from "./ChatInput";
-import { useCsrfToken } from "@/packages/shared/src/hooks";
-import { useChat } from "ai/react";
-import { useQueryClient } from "@tanstack/react-query";
 import Messages from "./Messages";
 import { useAccount } from "wagmi";
 import useConnectWallet from "@/hooks/useConnectWallet";
+import { useChatProvider } from "@/providers/ChatProvider";
 
 const Chat = () => {
   const { connectWallet } = useConnectWallet();
   const { address } = useAccount();
-  const csrfToken = useCsrfToken();
-  const accountId = "3664dcb4-164f-4566-8e7c-20b2c93f9951";
-  const queryClient = useQueryClient();
   const {
     messages,
     input,
     handleInputChange,
     handleSubmit: handleChatSubmit,
-  } = useChat({
-    api: `/api/chat`,
-    headers: {
-      "X-CSRF-Token": csrfToken,
-    },
-    body: {
-      accountId,
-    },
-    onError: console.error,
-    onFinish: () => {
-      void queryClient.invalidateQueries({
-        queryKey: ["credits", accountId],
-      });
-    },
-  });
+  } = useChatProvider();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    console.log("Submit", address);
     if (!address) {
       e.preventDefault();
       await connectWallet();
