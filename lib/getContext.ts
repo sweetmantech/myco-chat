930c48982b5974b53d05310c6410611360a9d256
Sweets Sweetman <sweetmantech@gmail.com>
2024-09-30 21:29:24 -0300

    Increasing context.

--- lib/getContext.ts
+++ lib/getContext.ts
@@ -10,7 +10,7 @@ const getContext = async (address: string) => {
     const score = await scoreResponse.json();
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const processedTokens = tokens.tokens.map((token: any) => ({
+    const zoraTokens = tokens.tokens.map((token: any) => ({
         event: token.event,
         address: token.address,
         timestamp: token.timestamp,
@@ -19,11 +19,18 @@ const getContext = async (address: string) => {
         tokenId: token.metadata.tokenId,
         collection: token.metadata.collection,
         transactionHash: token.metadata.transactionHash
-    }));
+    })).slice(0, 7);
+    const zoraScore = {
+        address: score.address,
+        score: score.score,
+        completeness: score.profile.completeness,
+        followers: score.profile.followers,
+        following: score.profile.following
+    }
 
     const context = {
-        zora_tokens: processedTokens.slice(0, 5),
-        zora_score: score
+        zoraTokens,
+        zoraScore
     };
 
     return context;
