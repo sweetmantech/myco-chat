91220f395d2c9d537e1bc9ce87fc48eb4c05f5be
Sweets Sweetman <sweetmantech@gmail.com>
2024-10-02 15:06:06 -0300

    I add login points tracking.

---
+++ lib/consts.ts
@@ -0,0 +1,3 @@
+// STACK EVENTS
+export const FIRST_SMART_WALLET_LOGIN_EVENT = 'first_smart_wallet_login'
+export const SMART_WALLET_LOGIN_POINT = 11
--- lib/stack/trackLoginPoints.tsx
+++ lib/stack/trackLoginPoints.tsx
@@ -1,14 +1,17 @@
-import { Address } from 'viem'
-import getStackClient from './getStackClient'
-import { FIRST_SMART_WALLET_LOGIN_EVENT, SMART_WALLET_LOGIN_POINT } from '../consts'
+import { Address } from "viem";
+import getStackClient from "./getStackClient";
+import {
+  FIRST_SMART_WALLET_LOGIN_EVENT,
+  SMART_WALLET_LOGIN_POINT,
+} from "../consts";
 
 const trackLoginPoints = async (address: Address) => {
-  const stackClient = getStackClient()
+  const stackClient = getStackClient();
   await stackClient.track(FIRST_SMART_WALLET_LOGIN_EVENT, {
     points: SMART_WALLET_LOGIN_POINT,
     account: address,
     uniqueId: address,
-  })
-}
+  });
+};
 
-export default trackLoginPoints
+export default trackLoginPoints;
