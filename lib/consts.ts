8b34552aae92093ff8708a140a49ee81372017a1
Sweets Sweetman <sweetmantech@gmail.com>
2024-10-02 21:37:08 -0300

    I consolidate multiple event names to a single event.

--- lib/consts.ts
+++ lib/consts.ts
@@ -1,7 +1,6 @@
 // STACK EVENTS
 export const FIRST_SMART_WALLET_LOGIN_EVENT = 'first_smart_wallet_login'
-export const USER_MESSAGE_SENT_EVENT = 'user_message_sent'
-export const ASSISTANT_MESSAGE_SENT_EVENT = 'assistant_message_sent'
+export const MESSAGE_SENT_EVENT = 'message_sent'
 export const SMART_WALLET_LOGIN_POINT = 11
 export const MESSAGE_SENT_POINT = 1
 export const CHAT_POINT_SYSTEM_ID = 4026
