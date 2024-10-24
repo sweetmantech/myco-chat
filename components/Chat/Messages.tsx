9bbb977f2efe5d65acdfe144ecf6b27c08d24be1
aidenwong812 <wongaiden812@gmail.com>
2024-10-24 15:50:02 +0800

    feat: support markdown

--- components/Chat/Messages.tsx
+++ components/Chat/Messages.tsx
@@ -1,5 +1,6 @@
 import { Message } from "ai";
 import { TvMinimalPlay } from "lucide-react";
+import { marked } from "marked";
 import useProfileSearch from "@/hooks/useProfileSearch";
 
 const Messages = ({ messages }: { messages: Message[] }) => {
@@ -11,28 +12,31 @@ const Messages = ({ messages }: { messages: Message[] }) => {
         {messages.map((message: Message, index: number) => (
           <div
             key={index}
-            className={`${message.role === "assistant" ? "flex" : ""}`}
+            className={message.role === "assistant" ? "flex" : ""}
           >
-            {
-              message.role === "assistant" && (
-                <div className="w-8 h-8">
-                  {
-                    profile.length > 0 ? (
-                      <img src={`https://zora.co/api/avatar/${profile[0].address}`} alt="PFP" width={36} height={36} className="rounded-full" />
-                    ) : (
-                      <TvMinimalPlay size={32} color="#000000" />
-                    )
-                  }
-                </div>
-              )
-            }
+            {message.role === "assistant" && (
+              <div className="w-8 h-8">
+                {profile.length > 0 ? (
+                  <img
+                    src={`https://zora.co/api/avatar/${profile[0].address}`}
+                    alt="PFP"
+                    width={36}
+                    height={36}
+                    className="rounded-full"
+                  />
+                ) : (
+                  <TvMinimalPlay size={32} color="#000000" />
+                )}
+              </div>
+            )}
             <div
-              className={`p-3 rounded-lg ${message.role === "user"
-                ? "bg-black text-white w-1/2 float-right"
-                : "bg-transparent text-black w-[90%]"
-                }`}
+              className={`p-3 rounded-lg ${
+                message.role === "user"
+                  ? "bg-black text-white max-w-1/2 float-right"
+                  : "bg-transparent text-black w-[90%]"
+              }`}
             >
-              <p className="text-sm">{message.content}</p>
+              <p className="text-sm" dangerouslySetInnerHTML={{ __html: marked.parse(message.content, { async: false }) as string }} />
             </div>
           </div>
         ))}
