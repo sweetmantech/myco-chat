bf29d85af0b0f710e9d41bb7a970660a7b779971
aidenwong812 <wongaiden812@gmail.com>
2024-10-30 22:45:28 +0800

    fix bug

--- lib/wagmi/config.ts
+++ lib/wagmi/config.ts
@@ -1,15 +1,14 @@
 import { createConfig, http } from 'wagmi'
-import { coinbaseWallet, metaMask } from 'wagmi/connectors'
+import { coinbaseWallet } from 'wagmi/connectors'
 import { base, baseSepolia } from 'viem/chains'
 
 const wagmiConfig = createConfig({
   chains: [base, baseSepolia],
   connectors: [
-    // coinbaseWallet({
-    //   appName: 'myco.wtf',
-    //   preference: 'smartWalletOnly',
-    // }),
-    metaMask({}),
+    coinbaseWallet({
+      appName: 'myco.wtf',
+      preference: 'smartWalletOnly',
+    }),
   ],
   transports: {
     [base.id]: http(),
