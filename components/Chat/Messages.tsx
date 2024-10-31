c493dafd375b9a3d762f18aa8603666fa4e316cb
aidenwong812 <wongaiden812@gmail.com>
2024-10-31 16:28:11 +0800

    refactor with react-virtuoso

--- components/Chat/Messages.tsx
+++ components/Chat/Messages.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
-import { Message } from "ai";
 import { TvMinimalPlay } from "lucide-react";
 import ReactMarkdown from "react-markdown";
+import { Virtuoso } from 'react-virtuoso';
 import useProfileSearch from "@/hooks/useProfileSearch";
 import getZoraPfpLink from "@/lib/zora/getZoraPfpLink";
 import { useChatProvider } from "@/providers/ChatProvider";
@@ -10,55 +10,62 @@ import Thinking from "./Thinking";
 const Messages = () => {
   const { messages, pending } = useChatProvider();
   const { profile } = useProfileSearch();
-  const scrollRef = useRef<HTMLDivElement>(null);
+  const virtuoso = useRef(null);
 
   useEffect(() => {
-    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+    if (virtuoso.current) {
+      virtuoso.current.scrollToIndex({ index: messages.length, behavior: "auto", align: "end" });
+    }
   });
 
   return (
-    <div className="w-full max-w-xl mt-4 mb-2 overflow-y-auto">
-      <div className="space-y-4 flex flex-col">
-        {messages.map((message: Message, index: number) => message.content && (
-          <div
-            key={index}
-            className={message.role === "assistant" ? "flex" : ""}
-          >
-            {
-              message.role === "assistant" && (
-                <div className="w-8 h-8">
-                  {
-                    profile.length > 0 ? (
-                      <img
-                        src={getZoraPfpLink(profile[0])}
-                        alt="PFP"
-                        width={36}
-                        height={36}
-                        className="rounded-full"
-                      />
-                    ) : (
-                      <TvMinimalPlay size={32} color="#000000" />
-                    )
-                  }
+    <div className="w-full max-w-xl mt-4 mb-2">
+      <Virtuoso
+        style={{ height: pending ? "calc(100vh - 350px)" : "calc(100vh - 320px)" }}
+        data={messages}
+        totalCount={messages.length}
+        ref={virtuoso}
+        itemContent={(_, message) => {
+          return (
+            <div className={`w-full flex ${message.role === "user" ? "justify-end" : "justify-start"}`}>
+              <div
+                className={`w-fit ${message.role === "assistant" ? "flex" : "max-w-[85%]"} h-fit mb-2`}
+              >
+                {
+                  message.role === "assistant" && (
+                    <div className="w-8 h-8">
+                      {
+                        profile.length > 0 ? (
+                          <img
+                            src={getZoraPfpLink(profile[0])}
+                            alt="PFP"
+                            width={36}
+                            height={36}
+                            className="rounded-full"
+                          />
+                        ) : (
+                          <TvMinimalPlay size={32} color="#000000" />
+                        )
+                      }
+                    </div>
+                  )
+                }
+                <div
+                  className={`p-3 rounded-lg ${message.role === "user"
+                    ? "bg-black text-white w-fit"
+                    : "flex-1 bg-transparent text-black"
+                    }`}
+                >
+                  <ReactMarkdown className="text-sm">
+                    {message.content}
+                  </ReactMarkdown>
                 </div>
-              )
-            }
-            <div
-              className={`p-3 rounded-lg ${
-                message.role === "user"
-                  ? "bg-black text-white float-right max-w-[85%]"
-                  : "flex-1 bg-transparent text-black"
-              }`}
-            >
-              <ReactMarkdown className="text-sm">
-                {message.content}
-              </ReactMarkdown>
+              </div>
             </div>
-          </div>
-        ))}
-        {pending && <Thinking />}
-      </div>
-      <div ref={scrollRef} />
+          )
+        }}
+      />
+      {pending && <Thinking />}
     </div>
   );
 };
