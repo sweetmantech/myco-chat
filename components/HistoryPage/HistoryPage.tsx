19a532d07e283a001722a90937efad452d6eeaf8
aidenwong812 <wongaiden812@gmail.com>
2024-11-12 23:49:28 -0500

    fix scroll

--- components/HistoryPage/HistoryPage.tsx
+++ components/HistoryPage/HistoryPage.tsx
@@ -6,8 +6,8 @@ const HistoryPage = () => {
 
   return (
     <div className="flex font-nounish flex-col h-[100vh] bg-background border border-black">
-      <main className="flex-1 flex flex-col p-4 items-center">
-        <div className="flex flex-col items-center mt-24 space-y-4 w-[576px]">
+      <main className="flex-1 flex flex-col p-4 items-center h-full">
+        <div className="flex flex-col items-center mt-24 space-y-4 w-[576px] overflow-y-auto">
           {conversations.map((conversation) => (
             <Conversation
               key={conversation.metadata.id}
