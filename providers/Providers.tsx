b907c5890d58bff97d5d6e89aa686cd026a41018
codecraftwt <codecraftwt@gmail.com>
2024-12-03 12:45:34 +0530

    Added the privy provider and app id is set in .env file

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -3,15 +3,26 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChatProvider } from "./ChatProvider";
 import WagmiProvider from "./WagmiProvider";
+import { PrivyProvider } from "@privy-io/react-auth";
+
+const PRIVY_APP_ID = process.env.NEXT_PUBLIC_PRIVY_APP_ID;
+
+if (!PRIVY_APP_ID) {
+  throw new Error(
+    "Privy App ID is missing. Please set NEXT_PUBLIC_PRIVY_APP_ID in your .env file."
+  );
+}
 
 const queryClient = new QueryClient();
 
 const Providers = ({ children }: { children: React.ReactNode }) => (
-  <WagmiProvider>
-    <QueryClientProvider client={queryClient}>
-      <ChatProvider>{children}</ChatProvider>
-    </QueryClientProvider>
-  </WagmiProvider>
+  <PrivyProvider appId={PRIVY_APP_ID}>
+    <WagmiProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChatProvider>{children}</ChatProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </PrivyProvider>
 );
 
 export default Providers;
