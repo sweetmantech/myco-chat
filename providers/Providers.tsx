98bbe2109f2bbb4551eba269f6d1a56afe811cb5
codecraftwt <codecraftwt@gmail.com>
2024-12-03 11:49:21 +0530

    Added Privy Provider

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -3,15 +3,20 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChatProvider } from "./ChatProvider";
 import WagmiProvider from "./WagmiProvider";
+import { PrivyProvider } from "@privy-io/react-auth";
+
+const PRIVY_APP_ID = "cm46y5zd8012byzh6hdqcvvvg";
 
 const queryClient = new QueryClient();
 
 const Providers = ({ children }: { children: React.ReactNode }) => (
-  <WagmiProvider>
-    <QueryClientProvider client={queryClient}>
-      <ChatProvider>{children}</ChatProvider>
-    </QueryClientProvider>
-  </WagmiProvider>
+  <PrivyProvider appId={PRIVY_APP_ID}>
+    <WagmiProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChatProvider>{children}</ChatProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </PrivyProvider>
 );
 
 export default Providers;
