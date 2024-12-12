7da2ee9e67da7ba0303e6964850a3c4ca233a43c
Nathaniyell <essien.nathan@yahoo.com>
2024-12-12 13:52:43 +0100

    Add Install Button

---
+++ components/Button/InstallButton.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export function InstallButton() {
+
+  return (
+    <button
+      className="fixed !top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black text-background px-6 py-3 rounded-lg shadow-lg font-bold"
+    >
+      Add to Home Screen
+    </button>
+  );
+} 
