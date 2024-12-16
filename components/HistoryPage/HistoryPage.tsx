451a26d212020a2291d5df7338b19e83cd9cb9e2
Nathaniyell <essien.nathan@yahoo.com>
2024-12-16 18:24:50 +0100

    update chat history width and stop horizontal scrolling

--- components/HistoryPage/HistoryPage.tsx
+++ components/HistoryPage/HistoryPage.tsx
@@ -7,7 +7,7 @@ const HistoryPage = () => {
   return (
     <div className="flex font-nounish flex-col h-[100vh] bg-background border border-black">
       <main className="flex-1 flex flex-col p-4 items-center">
-        <div className="flex flex-col items-center mt-24 space-y-4 w-[576px]">
+        <div className="flex flex-col items-center mt-24 space-y-4 w-11/12 mx-auto">
           {conversations.map((conversation) => (
             <Conversation
               key={conversation.metadata.id}
