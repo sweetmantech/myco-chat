7066e0e98b6ce3dd8ce5b3d4aeb8e54897506e30
Nathaniyell <essien.nathan@yahoo.com>
2024-12-17 10:17:46 +0100

    update the modal styles

--- components/Button/AppDownloadModal.tsx
+++ components/Button/AppDownloadModal.tsx
@@ -14,20 +14,23 @@ export function AppDownloadModal() {
   }
 
   return (
-    <div className='fixed font-nounish text-black !top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col space-y-4  w-8/12 mx-auto bg-transparent'>
-      <div className='rounded-3xl border border-black p-4 flex flex-col space-y-10 shadow-2xl'>
+    <div className='fixed font-nounish text-black !top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col space-y-4  w-[90%] mx-auto bg-transparent'>
+      <div className='rounded-3xl border border-black p-4 flex flex-col space-y-10 shadow-[2px_2px_2px_rgba(0,0,0,0.8)]'>
         <div className='flex flex-col space-y-2'>
-          <h2 className='text-2xl font-bold'>Download the app</h2>
-          <p className='text-lg font-semibold'>For the best experience, download the app.</p>
+          <h2 className='text-lg'>Download the app</h2>
+          <p className='text-2xl'>For the best experience, download the app.</p>
         </div>
         <button
           onClick={promptToInstall}
-          className=" bg-black text-white px-6 py-3 font-bold rounded-full"
+          className=" bg-black text-white px-6 py-3 w-[260px] rounded-full mx-auto"
         >
           Download
         </button>
       </div>
-      <button onClick={() => setIsInstallable(false)} className='text-black rounded-full border border-black px-6 py-3 font-bold shadow-2xl'>Not Now</button>
+      
+
+      <button onClick={() => setIsInstallable(false)} className='text-black rounded-full border border-black px-6 py-3  shadow-[2px_2px_2px_rgba(0,0,0,0.8)] w-[260px] mx-auto'>Not Now</button>
+    
     </div>
   );
 } 
